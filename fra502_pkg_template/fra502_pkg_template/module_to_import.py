c6cf530927112a99791140cb5dd11cd1307420ae
aminballoon <aminballoon@hotmail.com>
2022-03-14 09:42:44 +0700

    Update pkg template

---
+++ fra502_pkg_template/fra502_pkg_template/module_to_import.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+def fra502_test_pkg(text):
+    return "fra502 " + text 
---
+++ fra502_pkg_template/src/cpp_node.cpp
@@ -0,0 +1,8 @@
+#include "fra502_pkg_template/cpp_header.hpp"
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!\n";
+    return 0;
+}
