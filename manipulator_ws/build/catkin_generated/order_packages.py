290a89cda067b10952fc15ae9b085c4d51812db6
Norawit Nangsue <norawitn@hotmail.com>
2022-06-02 12:16:55 +0700

    remove junks

--- manipulator_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/oak/502/manipulator_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/oak/502/manipulator_ws/devel;/home/oak/mbse_ws/devel;/home/oak/rrrrrr_ws/devel;/home/oak/thesis_ws/devel;/home/oak/journey_ws/devel;/home/oak/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/oak/502/manipulator_ws/devel;/home/oak/mbse_ws/devel;/home/oak/rrrrrr_ws/devel;/home/oak/thesis_ws/devel;/home/oak/journey_ws/devel;/home/oak/catkin_ws/devel;/opt/ros/noetic' != '' else []
